71fbba1764b67104ae3697c130f8379543be8325
Richard Abanto <richardabanto@MacBook-Pro-de-Richard-8662.local>
2023-11-25 11:39:19 -0500

    subiendo cambios

---
+++ clase-2/anonymouse.js
@@ -0,0 +1,25 @@
+function saludar(){
+    console.log("hola");
+    return "mundo";
+}
+
+var saludar = function(){
+    console.log("otro saludo");
+};
+
+
+
+console.log(saludar());
+
+function func2(arg, fn){
+    fn(arg);
+}
+
+function saludar2(nom)
+{
+    console.log("Hola "+nom)
+}
+(function(){
+    console.log("estyoy dentro de una funcion")
+})();
+
---
+++ clase-2/reto.js
@@ -0,0 +1,3 @@
+require("prompt");
+
+var num = prompt("Ingrese un numero: ");
